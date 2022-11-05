717a86877f34c619fafef6294d787d3143b9c5d9
sergiomuguruza <sergiomuguruza@gmail.com>
2022-11-04 22:31:22 -0300

    actualizo bbdd

--- BBDD/bbdd_bienes_raices_future_estado.sql
+++ BBDD/bbdd_bienes_raices_future_estado.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `estado`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `estado` (
-  `Id_Estado` int NOT NULL,
+  `Id_Estado` int NOT NULL AUTO_INCREMENT,
   `Nombre_Estado` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id_Estado`),
   UNIQUE KEY `Id_Estado_UNIQUE` (`Id_Estado`),
